78ff01a858bcd3dc6a2e7e8b832d713b10b91de1
Tom van Nimwegen <tvn@ttt.nl>
2017-06-14 17:03:17 +0200

    barrel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,7 @@ import { MyApp } from './app.component'
 
 import { HttpModule } from '@angular/http'
 
-import { AboutPage } from '../pages/about/about'
-import { ContactPage } from '../pages/contact/contact'
-import { HomePage } from '../pages/home/home'
-import { TabsPage } from '../pages/tabs/tabs'
+import { pages }from '../pages'
 
 import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
@@ -18,10 +15,7 @@ import { HomeService } from './services/home.service'
 @NgModule({
 	declarations: [
 		MyApp,
-		AboutPage,
-		ContactPage,
-		HomePage,
-		TabsPage
+		...pages
 	],
 	imports: [
 		BrowserModule,
@@ -31,10 +25,7 @@ import { HomeService } from './services/home.service'
 	bootstrap: [IonicApp],
 	entryComponents: [
 		MyApp,
-		AboutPage,
-		ContactPage,
-		HomePage,
-		TabsPage
+		...pages
 	],
 	providers: [
 		StatusBar,
---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+import { TabsPage } from './tabs/tabs'
+import { HomePage } from './home/home'
+import { ContactPage } from './contact/contact'
+
+export const pages = [
+	TabsPage,
+	HomePage,
+	ContactPage
+]
